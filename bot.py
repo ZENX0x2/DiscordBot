ddcdd53a621ac4323ec84059065b009c7be6f85b
Jonathan <46301474+Z3NX-Y@users.noreply.github.com>
2021-07-09 17:01:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import random
 import time
+import string
 from discord import channel
 import discord.ext
 from discord.utils import get
@@ -16,5 +17,18 @@ client = commands.Bot(command_prefix='-', case_insensitive=True)
 async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Nookavan On YouTube"))
 
+
+@client.command(pass_context=True, name="kick")
+@has_permissions(kick_members=True)
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+    await ctx.send(f'{member} Has been kicked.')
+
+
+@client.command(pass_context=True, name="ban")
+@has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'{member} Has been banned.')
 
 client.run("ODYzMDE5MDU5NjAyNzg0MjY3.YOgzIQ.-_HM3-DIXHNa15v5eGDdFISu-fQ")
